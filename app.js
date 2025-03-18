4e547a4138f90e15a1e2a894be8bb472dcb280c1
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-18 15:53:48 +0530

    kj

--- app.js
+++ app.js
@@ -47,17 +47,17 @@
 // })
 
 const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("P1 success"), 3000);
+  setTimeout(() => reject("P1 success"), 2000);
 });
 
 const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject("P2 Fail"), 1000);
+  setTimeout(() => reject("P2 sucess"), 1000);
 //   setTimeout(() => resolve("P2 success"), 1000);
 });
 
 const p3 = new Promise((resolve, reject) => {
 //   setTimeout(() => resolve("P3 success"), 2000);
-  setTimeout(() => reject("P3 success"), 2000);
+  setTimeout(() => reject("P3 fail"), 2000);
 });
 
 // Promise.all([p1,p2,p3]).then((res)=>{
@@ -67,7 +67,22 @@ const p3 = new Promise((resolve, reject) => {
 //     console.error(err)
 // })
 
-Promise.all([p1,p2,p3]).then((res)=>{
+// Promise.allSettled([p1,p2,p3]).then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.error(err)
+// })
+
+
+// Promise.race([p1,p2,p3]).then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.error(err)
+// })
+
+Promise.any([p1,p2,p3]).then((res)=>{
     console.log(res)
 })
 .catch((err)=>{
