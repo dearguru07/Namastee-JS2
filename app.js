eac96a1abf692c3a1f60d665a6860e608f4b2b96
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-18 15:43:29 +0530

    kk

--- app.js
+++ app.js
@@ -19,7 +19,7 @@
 
 // console.log('Hello world......');
 
-const Cart = ["pants", "shoes", "books"];
+// const Cart = ["pants", "shoes", "books"];
 
 // api.CreateOrder(Cart, function () {
 //   api.ProceedToPyment(function () {
@@ -36,12 +36,40 @@ const Cart = ["pants", "shoes", "books"];
 //   });
 // });
 
+// const Promis=CreateOrder(cart);
 
-CreateOrder(Cart).then((orderID)=>{
- return  proceedToPayment()
-}).then((paymentInfo)=>{
-  return showOrderSummary()
-}).then((paymentInfo)=>{
-  return updateWallet()
+// CreateOrder(Cart).then((orderID)=>{
+//  return  proceedToPayment()
+// }).then((paymentInfo)=>{
+//   return showOrderSummary()
+// }).then((paymentInfo)=>{
+//   return updateWallet()
+// })
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P1 success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("P2 Fail"), 1000);
+//   setTimeout(() => resolve("P2 success"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("P3 success"), 2000);
+  setTimeout(() => reject("P3 success"), 2000);
+});
+
+// Promise.all([p1,p2,p3]).then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.error(err)
+// })
+
+Promise.all([p1,p2,p3]).then((res)=>{
+    console.log(res)
 })
-
+.catch((err)=>{
+    console.error(err)
+})
